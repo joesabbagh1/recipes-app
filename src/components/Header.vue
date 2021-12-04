46078edc2d9578c920511d65325fc1ba34cb525b
Joe Sabbagh <joesabbagh12345@gmail.com>
2021-12-04 02:17:22 +0200

    recipe content

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+<v-card
+    flat
+    tile
+  >
+    <v-toolbar color="primary">
+      <v-toolbar-title>Logo</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <router-link to="/recipe-content">yo</router-link>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.background{
+    background-color: #C7EAE4;
+    height: 4rem;
+}
+</style>
---
+++ src/views/RecipeContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <RecipeContent :recipe='recipes[0]' />
+  </div>
+</template>
+
+<script>
+import RecipeContent from '../components/Recipe-content.vue';
+export default {
+    data: () => ({
+        recipes:[
+        {
+            title: 'pizza',
+            image: 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg',
+            ingrediants: [
+                '2 eggs',
+                '1kg flour',
+                '1 tb sugar',
+                '1 tb sugar',
+                '1 tb sugar',
+                '1 tb sugar',
+                '1 tb sugar',
+                '1 tb sugar',
+                '500g mozorella'
+            ]
+        }
+        ]
+  }),
+
+    components: {
+        RecipeContent
+    },
+}
+</script>
+
+<style>
+
+</style>
